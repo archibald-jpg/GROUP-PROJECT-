97ccf9f9828693b25a9fbbefdaf2a4da283163cc
rollantonn <anzton.rolland12@gmail.com>
2024-12-10 16:24:43 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
+//create a red circle of 100px in the middle of the canvas
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  noStroke();
+  fill("red");
+  circle(width / 2, height / 2, 100);
 }
