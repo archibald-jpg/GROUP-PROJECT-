852431bdc01703db87ddb89a02609e9259e48da8
rollantonn <anzton.rolland12@gmail.com>
2024-12-10 16:44:01 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,11 +12,17 @@ function draw() {
   fill("red");
   circle(width / 2, height / 2, 100);
 
-
   noStroke();
   fill("blue");
   circle(width / 4, height / 4, 300);
 
+  noStroke();
+  fill("yellow");
+  rect(100, 100, 100);
 
-
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < width; j++) {
+      circle(5 + 1 * 10, 5 + j * 12, noise(i, j, frameCount / 4) * 10);
+    }
+  }
 }
